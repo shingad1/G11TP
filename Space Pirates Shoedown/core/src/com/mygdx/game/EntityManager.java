3e3df3664d7594ce220161fb49916b49a0937050
Devin S <shingad1@aston,ac,uk>
2018-10-24 16:45:07 +0100

    SOMETHING POPPED UP
    Window of the game

---
+++ Space Pirates Shoedown/core/src/com/mygdx/game/EntityManager.java
@@ -0,0 +1,35 @@
+ /* package com.mygdx.game;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EntityManager {
+
+    private EntityManager entityManager;
+
+    public EntityManager(Engine e, SpriteBatch batch) {
+        private Engine engine;
+
+    }
+    public void create() {
+        Engine engine = new Engine();
+
+
+        batch = new SpriteBatch();
+        entityManager = new EntityManager(engine, batch);
+
+
+        batch.begin();
+        entityManager.update(); //after batch.begin
+
+
+    }
+
+
+    public void update() {
+
+    }
+
+}
+*/
--- Space Pirates Shoedown/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Space Pirates Shoedown/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.mygdx.game.SpacePiratesShoedown;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = ("Space Pirates Shoedown");
+		config.width = 100;
+		config.height = 100;
+		config.useGL30 = false;
+		config.resizable = false;
 		new LwjglApplication(new SpacePiratesShoedown(), config);
 	}
 }
