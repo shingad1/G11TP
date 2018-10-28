ff572d0e2f6ba3126b9f2bc4a836d078445afa58
Devin S <shingad1@aston,ac,uk>
2018-10-28 17:42:38 +0000

    changing size, title in Desktop Launcher

--- Space Pirates Shoedown/desktop/src/com/sps/game/desktop/DesktopLauncher.java
+++ Space Pirates Shoedown/desktop/src/com/sps/game/desktop/DesktopLauncher.java
@@ -8,5 +8,10 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new SpacePiratesShoedown(), config);
+		config.width = 640; //width of the window
+		config.height = 640; //height of the window;
+		config.title = ("Space Pirates Shoedown");
+		config.useGL30 = false;
+		config.resizable = false;
 	}
 }
