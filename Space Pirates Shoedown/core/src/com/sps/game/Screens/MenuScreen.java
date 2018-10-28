69c70edf5d69095604b3b0fbf0f07391f56c1af8
Miraj <miraj.shah@hotmail.co.uk>
2018-10-28 17:18:31 +0000

    Some changes

---
+++ Space Pirates Shoedown/core/src/com/sps/game/Screens/MenuScreen.java
@@ -0,0 +1,8 @@
+package com.sps.game.Screens;
+
+import com.badlogic.gdx.Screen;
+
+public class MenuScreen implements Screen {
+
+
+}
--- Space Pirates Shoedown/core/src/com/sps/game/SpacePiratesShoedown.java
+++ Space Pirates Shoedown/core/src/com/sps/game/SpacePiratesShoedown.java
@@ -1,28 +1,26 @@
 package com.sps.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SpacePiratesShoedown extends ApplicationAdapter {
+public class SpacePiratesShoedown extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+		setScreen(new MenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();//delegates menu method to whatever screen is active at the time
 	}
 	
 	@Override
