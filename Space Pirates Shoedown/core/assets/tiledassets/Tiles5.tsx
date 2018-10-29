5e155f627960610bedff04bdc7ade266b9b72821
Miraj <miraj.shah@hotmail.co.uk>
2018-10-29 21:14:05 +0000

    blocked titled

--- Space Pirates Shoedown/core/assets/tiledassets/Tiles5.tsx
+++ Space Pirates Shoedown/core/assets/tiledassets/Tiles5.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="Tiles5" tilewidth="32" tileheight="32" tilecount="490" columns="10">
  <image source="img/1/tilesheet.png" width="320" height="1568"/>
+ <tile id="56">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
 </tileset>
