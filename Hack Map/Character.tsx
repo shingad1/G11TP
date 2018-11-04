c75fc8e5eb64087cd5b5e070a5d3a9bd5244f485
Alex Bernini <Bernini98@users.noreply.github.com>
2018-11-04 19:39:16 +0000

    Hack Map Files

---
+++ Hack Map/Character.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Character" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="CorrectedCharacterSpritesheet.png" width="96" height="128"/>
+</tileset>
---
+++ Hack Map/Tiles5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Tiles5" tilewidth="32" tileheight="32" tilecount="490" columns="10">
+ <image source="img/1/tilesheet.png" width="320" height="1568"/>
+</tileset>
