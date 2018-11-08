05cb516f0be2b47dbac82d97f351b71a3c0a9d62
Alex Bernini <Bernini98@users.noreply.github.com>
2018-11-08 16:17:48 +0000

    Removing SplashAnimation class contents

--- Space Pirates Shoedown/core/src/com/sps/game/Animation/SplashAnimation.java
+++ Space Pirates Shoedown/core/src/com/sps/game/Animation/SplashAnimation.java
@@ -1,17 +1,2 @@
 package com.sps.game.Animation;
 
-import aurelienribon
-
-import com.badlogic.gdx.graphics.g2d.Sprite;
-
-public class SplashAnimation implements TweenAccessor<Sprite> {
-
-    public int getValues(Sprite target, int tweenType, float[] returnValues) {
-
-    }
-
-    public int setValues(Sprite target, int tweenType, float[] returnValues) {
-
-    }
-
-}
