b8de06ae86d9f368a7fa608cb62a2aa4b0eb9213
Miraj <miraj.shah@hotmail.co.uk>
2018-11-29 15:37:38 +0000

    update on candy land

---
+++ Space Pirates Shoedown/core/assets/tiledassets/CandyLand/candy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="candy" tilewidth="32" tileheight="32" tilecount="54" columns="9">
+ <image source="candy.png" width="300" height="200"/>
+</tileset>
