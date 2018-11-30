ec2a9e781450132f4b55f088cb9da78dfbbc9257
DenashS <denash8@gmail.com>
2018-11-30 14:29:29 +0000

    CandyLand
    - Map Changes
    - Pink Houses (New house variant)
    - Pink Tree

---
+++ Space Pirates Shoedown/core/assets/tiledassets/decoration_medieval/decoration_medieval/pinkhouse.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="pinkhouse" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="30" columns="6">
+ <image source="../../CandyLand/pinkrichhouse.png" width="214" height="182"/>
+</tileset>
---
+++ Space Pirates Shoedown/core/assets/tiledassets/decoration_medieval/decoration_medieval/pinktree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="pinktree" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="144" columns="12">
+ <image source="../../CandyLand/plant repack.png" width="416" height="416"/>
+</tileset>
