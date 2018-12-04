dd4441b2b5d1f8eb7cd639c2fdc304aec6547eec
Lainey <alainyawallace@aol.com>
2018-12-04 20:44:28 +0000

    apparently I have made 600 changes...

---
+++ Website/SPSD Jar/core/assets/tiledassets/CandyLand/candyLandTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="candyLandTiles" tilewidth="32" tileheight="32" tilecount="504" columns="28">
+ <image source="CandyLand1.jpg" width="914" height="587"/>
+</tileset>
---
+++ Website/SPSD Jar/core/assets/tiledassets/lpcforest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="lpcforest" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="225" columns="15">
+ <image source="../../../../Hack Submission/Space Pirates Shoedown/core/assets/tiledassets/img/LPC_forest/LPC_forest/forest_tiles.png" width="512" height="512"/>
+</tileset>
