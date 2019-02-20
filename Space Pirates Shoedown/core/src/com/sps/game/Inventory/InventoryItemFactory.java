b48da88ccde97a02f656ebca57b63fdd0a431443
Devin S <devin_shingadia11@hotmail.com>
2019-02-20 11:29:12 +0000

    Removed Errors

--- Space Pirates Shoedown/core/src/com/sps/game/Inventory/InventoryItemFactory.java
+++ Space Pirates Shoedown/core/src/com/sps/game/Inventory/InventoryItemFactory.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.Scaling;
 import com.sps.game.Utility;
+import com.sps.game.Inventory.InventoryItem.ItemTypeID;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 public class InventoryItemFactory {
 
@@ -17,7 +20,7 @@ public class InventoryItemFactory {
 
     private static InventoryItemFactory instance = null;
 
-    private Hashtable<ItemTypeID, InventoryItem> inventoryItemList;
+    private Hashtable<ItemTypeID, InventoryItem> _inventoryItemList;
 
     public static InventoryItemFactory getInstance(){
         if(instance == null){
@@ -28,15 +31,16 @@ public class InventoryItemFactory {
 
     private InventoryItemFactory(){
         ArrayList<JsonValue> list = json.fromJson(ArrayList.class, Gdx.files.internal(INVENTORY_ITEM));
-        HashMap <ItemTypeID, InventoryItem> inventoryItemList = new Hashtable<ItemTypeID, InventoryItem>();
+        _inventoryItemList = new Hashtable<ItemTypeID, InventoryItem>();
+
         for(JsonValue jsonVal : list){
             InventoryItem inventoryItem = json.readValue(InventoryItem.class, jsonVal);
-            inventoryItemList.put(inventoryItem.getItemTypeID(), inventoryItem);
+            _inventoryItemList.put(inventoryItem.getItemTypeID(), inventoryItem);
         }
     }
 
     public InventoryItem getInventoryItem(ItemTypeID inventoryItemType){
-        InventoryItem item = new InventoryItem(inventoryItemList.get(inventoryItemType));
+        InventoryItem item = new InventoryItem(_inventoryItemList.get(inventoryItemType));
         item.setDrawable(new TextureRegionDrawable(Utility.ITEMS_TEXTUREATLAS.findRegion(item.getItemTypeID().toString())));
         item.setScaling(Scaling.none);
         return item;
