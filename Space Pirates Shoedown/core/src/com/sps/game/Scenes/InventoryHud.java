2e0822a8a7344054eec012ce52dd8e7961beb861
Devin S <devin_shingadia11@hotmail.com>
2019-02-28 14:28:50 +0000

    Added drag and drop functionality to the inventory

--- Space Pirates Shoedown/core/src/com/sps/game/Scenes/InventoryHud.java
+++ Space Pirates Shoedown/core/src/com/sps/game/Scenes/InventoryHud.java
@@ -12,14 +12,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.sps.game.Controller.PlayerController;
-import com.sps.game.Inventory2.Inventory;
-import com.sps.game.Sprites.Player;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Payload;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Source;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Target;
 
-import java.util.ArrayList;
 
 public class InventoryHud {
 
@@ -41,24 +43,62 @@ public class InventoryHud {
 
         //In the future, change this to actual instances of class item.
         inventory.setItems("Axe",
-                           "Fuel",
-                           "Helmet",
-                           "Flux Capacitor",
-                           "Shoes",
-                           "Hamster",
-                           "Hammer",
-                           "Pirates Eye",
-                           "Cucumber");
+                "Fuel",
+                "Helmet",
+                "Flux Capacitor",
+                "Shoes",
+                "Hamster",
+                "Hammer",
+                "Pirates Eye",
+                "Cucumber");
 
 
         merchant.setItems("Shoe Laces",
-                          "Sword"
-                         );
+                "Sword"
+        );
+
+
+        DragAndDrop dnd = new DragAndDrop();
+        dnd.addSource(new DragAndDrop.Source(inventory) {
+            final Payload payload = new Payload();
+
+            @Override
+            public DragAndDrop.Payload dragStart(InputEvent inputEvent, float x, float y, int pointer) {
+                String item = inventory.getSelected();
+                payload.setObject(item);
+                inventory.getItems().removeIndex(inventory.getSelectedIndex());
+                payload.setDragActor(new Label(item, skin));
+                payload.setInvalidDragActor(new Label(item + " (\"No thanks!\")", skin));
+                payload.setValidDragActor(new Label(item + " (\"I'll buy this!\")", skin));
+                return payload;
+            }
+
+
+            @Override
+            public void dragStop(InputEvent event, float x, float y, int pointer, Payload payload, Target target) {
+                if (target == null) {
+                    inventory.getItems().add((String) payload.getObject());
+                }
+            }
+        });
+
+        dnd.addTarget(new Target(merchant) {
+            @Override
+            public boolean drag(Source source, Payload payload, float x, float y, int pointer) {
+                return !"Cucumber".equals(payload.getObject());
+            }
+
+            @Override
+            public void drop(Source source, Payload payload, float x, float y, int pointer) {
+                merchant.getItems().add((String) payload.getObject());
+            }
+        });
     }
 
     private void formatting() {
         stage = new Stage();
         Table table = new Table(skin);
+        table.defaults();
         table.center();
         table.setFillParent(true);
         table.add("Player").row();
@@ -67,6 +107,29 @@ public class InventoryHud {
         table.add(merchant);
 
         stage.addActor(table);
+
+
+        /*
+            Table inventoryTable = new Table(skin);
+            inventoryTable.setDebug(true);
+//          inventoryTable.setBackground();
+            inventoryTable.padRight(250);
+            inventoryTable.setFillParent(true);
+            inventoryTable.add("Player").row();
+            inventoryTable.add(inventory);
+
+            inventoryTable.add("Test");
+
+            stage.addActor(inventoryTable);
+
+            Table merchantTable = new Table(skin);
+            merchantTable.padLeft(250);
+            merchantTable.setFillParent(true);
+            merchantTable.add("Merchant").row();
+            merchantTable.add(merchant);
+            stage.addActor(merchantTable);
+    */
+
     }
 
 
