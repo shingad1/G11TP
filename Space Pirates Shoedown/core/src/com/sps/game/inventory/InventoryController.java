57c319c417ff12ec5c8096927a6b861b242e349a
Devin S <devin_shingadia11@hotmail.com>
2019-03-05 15:55:25 +0000

    Loading of images, getting there
    Item class now holds an image object
    Image objects of all items now in a list, held in the inventoryController
    That list returned by InventoryController used in InventoryHud.
    
    Next: Need to iterate through images list and add them to the stage, and sort out positioning in the table

--- Space Pirates Shoedown/core/src/com/sps/game/inventory/InventoryController.java
+++ Space Pirates Shoedown/core/src/com/sps/game/inventory/InventoryController.java
@@ -1,6 +1,7 @@
 package com.sps.game.inventory;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
@@ -12,6 +13,9 @@ public class InventoryController {
 
     //The list that will be holding the merchants items
     public List<String> merchantInventory = new List<String>(skin);
+
+    //The list that holds the image of each item
+    public List<Image> itemImageList = new List<Image>(skin);
 
     //List of all available items in the game
     public Item fuel;
@@ -46,16 +50,16 @@ public class InventoryController {
 
     //Setting the values of the items
     private void setItems() {
-        fuel = new Item("Fuel", "Fuel for your rocket shoes", 1);
-        axe = new Item("Axe", "An Axe to cut pirates in half", 2);
-        fluxCapacitor = new Item("Flux Capacitor", "Travel through time!", 3);
-        shoes = new Item("Shoes", "Some fake shoes", 4);
-        hamster = new Item("Hamster", "A rare hamster", 5);
-        hammer = new Item("Hammer", "1.5x more effective at killing pirates", 6);
-        piratesEye = new Item("Pirates Eye", "Gives the ability to spot pirates a mile away!", 7);
-        cucumber = new Item("Cucumber", "A useless cucumber", 8);
-        shoeLaces = new Item("Shoe Laces", "Some common shoe laces. Not edible", 9);
-        sword = new Item("Sword", "Hack your way to the final boss", 10);
+        fuel = new Item("Fuel", "Fuel for your rocket shoes", 1,  "core/assets/Inventory/images/sword.png");
+        axe = new Item("Axe", "An Axe to cut pirates in half", 2, "core/assets/Inventory/images/sword.png");
+        fluxCapacitor = new Item("Flux Capacitor", "Travel through time!", 3, "core/assets/Inventory/images/sword.png");
+        shoes = new Item("Shoes", "Some fake shoes", 4, "core/assets/Inventory/images/sword.png");
+        hamster = new Item("Hamster", "A rare hamster", 5, "core/assets/Inventory/images/sword.png");
+        hammer = new Item("Hammer", "1.5x more effective at killing pirates", 6, "core/assets/Inventory/images/sword.png");
+        piratesEye = new Item("Pirates Eye", "Gives the ability to spot pirates a mile away!", 7, "core/assets/Inventory/images/sword.png");
+        cucumber = new Item("Cucumber", "A useless cucumber", 8, "core/assets/Inventory/images/sword.png");
+        shoeLaces = new Item("Shoe Laces", "Some common shoe laces. Not edible", 9, "core/assets/Inventory/images/sword.png");
+        sword = new Item("Sword", "Hack your way to the final boss", 10, "core/assets/Inventory/images/sword.png");
     }
 
     public List<String> getInventoryList() {
@@ -65,6 +69,8 @@ public class InventoryController {
     public List<String> getMerchantList() {
         return merchantInventory;
     }
+
+    public List<Image> getImageList() { return itemImageList; }
 
 
 }
