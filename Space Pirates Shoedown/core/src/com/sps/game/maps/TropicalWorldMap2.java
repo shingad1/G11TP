f80ed200e380fe1cf867e4cc99edf5d09e5bf2ef
Miraj <miraj.shah@hotmail.co.uk>
2019-03-06 16:54:20 +0000

    Fixed

--- Space Pirates Shoedown/core/src/com/sps/game/maps/TropicalWorldMap2.java
+++ Space Pirates Shoedown/core/src/com/sps/game/maps/TropicalWorldMap2.java
@@ -5,7 +5,7 @@ public class TropicalWorldMap2 extends Map {
     /**
      * Holds the file path of the Home World map
      */
-    private static String mapPath = ASSETS_PATH + "HomeWorld/HomeWorldMap2.tmx";//tropical map here
+    private static String mapPath = ASSETS_PATH + "TropicalWorld/TropicalWorldMap2.tmx";//tropical map here
 
     public TropicalWorldMap2() {
         super(MapFactory.MapType.TropicalWorld2, mapPath);
