e6c9d5058f823fc9ebd7640a39dfaa46948e6b78
DenashS <denash8@gmail.com>
2019-03-10 17:03:23 +0000

    Candy Map Interior 1
    More Resources added / improved
    Edits made to Graphics

--- Space Pirates Shoedown/core/assets/tiledassets/CandyLand/House Interior/Resources/Classic Interior Objects.tsx
+++ Space Pirates Shoedown/core/assets/tiledassets/CandyLand/House Interior/Resources/Classic Interior Objects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="Classic Interior Objects" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../../HomeWorld/HouseInterior/Resources/Interior.png" width="1024" height="1024"/>
+<tileset version="1.2" tiledversion="1.2.2" name="Classic Interior Objects" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../HomeWorld/HouseInterior/Resources/Interior.png" width="1024" height="1024"/>
 </tileset>
