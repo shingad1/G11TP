86d5623f9fc10cbeee4fa8524a9767af96f679ad
SonicStarx <alainyawallace@aol.com>
2019-03-17 17:13:16 +0000

    created BasicEnemyTest

---
+++ Space Pirates Shoedown/core/test/com/sps/game/Sprites/BasicEnemyTest.java
@@ -0,0 +1,97 @@
+package com.sps.game.Sprites;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sps.game.animation.EnemyAnimation;
+import com.sps.game.maps.MapFactory;
+import com.sps.game.sprites.BasicEnemy;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class BasicEnemyTest {
+
+    private SpriteBatch sb;
+    private HashMap fightAnimation;
+    private BasicEnemy basicEnemy;
+    private String state;
+    private int health;
+    private int defence;
+
+    @Before
+    public void setUp() {
+        basicEnemy = new BasicEnemy(12,15, MapFactory.MapType.CandyWorld1,sb,"Boss");
+        fightAnimation = new HashMap<String, EnemyAnimation>();
+        fightAnimation.put("Idle",new EnemyAnimation(sb, basicEnemy, "enemyIdle.atlas", 1/15f));
+        health = 100;
+        int defence = 20;
+    }
+
+    @Test
+    public void getFightAnimation() {
+        assertEquals(fightAnimation,basicEnemy.getFightAnimation());
+    }
+
+    @Test
+    public void getWorld() {
+        assertEquals(MapFactory.MapType.CandyWorld1, basicEnemy.getWorld());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Boss",basicEnemy.getName());
+    }
+
+    @Test
+    public void changeState() {
+        private String newState = "run";
+        state = newState;
+        assertEquals(newState, state);
+    }
+
+    @Test
+    public void getX() {
+        assertEquals(12,basicEnemy.getX());
+    }
+
+    @Test
+    public void getY() {
+        assertEquals(15,basicEnemy.getY());
+    }
+
+    @Test
+    public void getVelocity() {
+    }
+
+    @Test
+    public void getAnimation() {
+        assertEquals("idle");
+    }
+
+    @Test
+    public void getHealth() {
+        assertEquals(100,basicEnemy.getHealth());
+    }
+
+    @Test
+    public void battleMove() {
+        //method currently empty
+    }
+
+    @Test
+    public void changeDefence() {
+
+    }
+
+    @Test
+    public void changeAttack() {
+    }
+
+    @Test
+    public void decreaseHealth() {
+    }
+}
---
+++ Space Pirates Shoedown/core/test/com/sps/game/Sprites/InteractiveNPCTest.java
@@ -0,0 +1,52 @@
+package com.sps.game.Sprites;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InteractiveNPCTest {
+
+    @Test
+    public void getAnimation() {
+    }
+
+    @Test
+    public void getWorld() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getType() {
+    }
+
+    @Test
+    public void getX() {
+    }
+
+    @Test
+    public void getY() {
+    }
+
+    @Test
+    public void getVelocity() {
+    }
+
+    @Test
+    public void setY() {
+    }
+
+    @Test
+    public void setX() {
+    }
+
+    @Test
+    public void getLocation() {
+    }
+
+    @Test
+    public void changeState() {
+    }
+}
