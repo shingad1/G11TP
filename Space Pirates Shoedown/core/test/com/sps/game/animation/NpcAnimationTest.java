f7a8747b10a518db6eeebc755c23fa416232f849
SonicStarx <alainyawallace@aol.com>
2019-03-17 13:53:56 +0000

    importing antonio's amendments

---
+++ Space Pirates Shoedown/core/test/com/sps/game/animation/NpcAnimationTest.java
@@ -0,0 +1,14 @@
+package com.sps.game.animation;
+import static org.junit.Assert.*;
+
+public class NpcAnimationTest {
+
+    @org.junit.Test
+    public void dispose() {
+        fail("fail");
+    }
+
+    @org.junit.Test
+    public void render() {
+    }
+}
