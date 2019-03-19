986d33c06ae7024676cd0c46449d214c152a617d
Miraj <miraj.shah@hotmail.co.uk>
2019-03-19 22:09:34 +0000

    need to test controls

--- Space Pirates Shoedown/core/src/com/sps/game/screens/ControlsScreen.java
+++ Space Pirates Shoedown/core/src/com/sps/game/screens/ControlsScreen.java
@@ -5,9 +5,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.sps.game.SpacePiratesShoedown;
+import com.sps.game.Utility;
 
 /**
  * This class displays the controls of the game to the user.
@@ -34,7 +39,7 @@ public class ControlsScreen implements Screen {
 
     private OrthographicCamera gamecam;
 
-    public ControlsScreen(SpacePiratesShoedown game){
+    public ControlsScreen(final SpacePiratesShoedown game){
         this.game = game;
         stage = new Stage();
         batch = new SpriteBatch();
@@ -43,6 +48,25 @@ public class ControlsScreen implements Screen {
         Gdx.input.setInputProcessor(stage);
         controlsTexture = new Texture(Gdx.files.internal("core/assets/MenuResources/Controls1.png"));
         Table table = new Table();
+        TextButton backButton = new TextButton("Back", Utility.STATUSUI_SKIN);
+
+        table.setHeight(backButton.getHeight());
+        table.setWidth(Gdx.graphics.getWidth());
+        table.add(backButton);
+        stage.addActor(table);
+
+        backButton.addListener(new ClickListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float a, float y, int pointer, int button){
+                dispose();
+                game.setScreen(new MenuScreen(game));
+            }
+        });
 
     }
 
