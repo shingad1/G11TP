51a80c05a1512c84835226fd6a1714ee63075f58
DenashS <denash8@gmail.com>
2019-03-20 18:04:12 +0000

    Graveyard Interior update

---
+++ Space Pirates Shoedown/core/assets/tiledassets/CandyLand/castleWalls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="castleWalls" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="../Graveyard/Resources/House Interior/castleWalls.png" width="512" height="480"/>
+</tileset>
