2efce86be6c3a6c6d84e4d6eda6f5d503a9ab3af
DenashS <denash8@gmail.com>
2019-03-20 12:23:52 +0000

    Graveyard Changes

---
+++ Space Pirates Shoedown/core/assets/tiledassets/Graveyard/graveMarkers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="graveMarkers" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="Resources/graveMarkers.png" width="224" height="224"/>
+</tileset>
