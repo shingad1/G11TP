5b927caab6fd61842ad02aefedee8991f19ef694
DenashS <denash8@gmail.com>
2019-03-20 18:58:15 +0000

    Graveyard 2nd Interior

---
+++ Space Pirates Shoedown/core/assets/tiledassets/Graveyard/castleWalls2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="castleWalls2" tilewidth="32" tileheight="32" tilecount="432" columns="24">
+ <image source="Resources/House Interior/castleWalls2.png" width="768" height="576"/>
+</tileset>
